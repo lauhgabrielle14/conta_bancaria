08f0d50e947b007c082f9d040f8611f28d0dbc18
lauhgabrielle14 <laurenfogaca_dev@hotmail.com>
2025-09-24 17:42:36 -0300

    :sparkles: Construção do Repository e Controller

--- src/conta_bancaria/repository/ContaRepository.java
+++ src/conta_bancaria/repository/ContaRepository.java
@@ -1,5 +1,17 @@
 package conta_bancaria.repository;
 
-public interface ContaRepository {
+import conta_bancaria.model.Conta;
 
+public interface ContaRepository {
+	//Métodos do CRUD
+	public void listarTodas ();
+	public void cadastrar (Conta conta);
+	public void atualizar (Conta conta);
+	public void procutarPorNumero (int numero);
+	public void deletar (int numero);
+	
+	//Métodos Bancários
+	public void sacar(int numero, float valor);
+	public void depositar(int numero, float valor);
+	public void transferir(int numeroOrigem, int numeroDestino, float valor);
 }
